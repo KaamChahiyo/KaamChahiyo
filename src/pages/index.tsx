3ff80172d8f518fa3cc564e0168f4d10d20238ca
anantamishra <mishraananta2017@gmail.com>
2022-12-30 00:16:12 +0545

    init

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <>
+      <div className="flex min-h-screen w-full justify-center items-center font-sans text-lg font-bold text-blue-700">
+        KaamChahiyo App
+      </div>
+    </>
+  )
+}
