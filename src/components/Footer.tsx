edb0e3ddfe40f748247f7a6630c9955238fe38bd
sumanchalise <sumanchalise57@gmail.com>
2023-01-14 20:53:49 +0545

    about

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,21 @@
 import React from "react";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="bg-[#19134def] text-gray-400 w-full">
+      <div className="flex">
+        <div className=" px-10 ">
+          About
+          <div className="">logo</div>
+          <div className="">About Text</div>
+        </div>
+        <div className=" px-20">Categories</div>
+        <div className=" px-20">Quick links</div>
+        <div className=" px-20">Location</div>
+      </div>
+      <div className="text-center  text-gray-50 ">
+        <span>Copyright © 2023 Apply. All rights reserved.</span>
+      </div>
+    </div>
+  );
 }
