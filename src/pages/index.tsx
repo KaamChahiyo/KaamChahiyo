8029baeec6ca8bce25b69d5e190acb79115bf924
Ananta Mishra <mishraananta2017@gmail.com>
2023-01-16 19:58:41 +0545

    added: AppHeader component with some icons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
+import AppHeader from "../components/AppHeader";
 import Footer from "../components/Footer";
 
 export default function Home() {
   return (
     <>
+      <AppHeader />
       <div className="flex min-h-screen w-full justify-center items-center font-sans text-lg font-bold text-blue-700">
         KaamChahiyo App
       </div>
