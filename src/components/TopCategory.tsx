cbd3ad55b7815a55d94bf681c8ac73a355549a53
Ananta Mishra <mishraananta2017@gmail.com>
2023-01-18 22:51:00 +0545

    section added

---
+++ src/components/TopCategory.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function TopCategory() {
+    return (
+        <div>Top Category</div>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import AppHeader from "../components/AppHeader";
 import Footer from "../components/Footer";
+import TopCategory from "../components/TopCategory";
+import TopServices from "../components/TopServices";
 
 export default function Home() {
   return (
@@ -11,8 +13,8 @@ export default function Home() {
           <Image
             src="/assets/img/snow-mountain.jpg"
             alt="Hero Section"
-            objectFit="cover"
-            layout="fill"
+            fill
+            className="object-cover"
             quality={100} />
           <div className="absolute top-80 left-44 w-full flex gap-10 m-auto container">
             <input className="px-8 py-3 rounded-full w-[50%] overflow-hidden focus:outline-none focus:shadow-outline focus:border-orange-400 border-2" type="text" placeholder="Search for a job by catagory" />
@@ -20,8 +22,8 @@ export default function Home() {
           </div>
         </div>
         <div>Recent Job posting</div>
-        <div>Top categories</div>
-        <div>Top Services</div>
+        <TopCategory />
+        <TopServices />
         <div>Top Employees</div>
         <div>Working procedure</div>
         <div>We have Served</div>
