0a02ba66736050e8aff8aa8c9550b649edefb4c4
Ananta Mishra <mishraananta2017@gmail.com>
2023-01-18 22:07:18 +0545

    added Hero section with search input fields

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import AppHeader from "../components/AppHeader";
 import Footer from "../components/Footer";
 
@@ -5,8 +6,19 @@ export default function Home() {
   return (
     <>
       <AppHeader />
-      <div className="">
-        <div>HeroSection</div>
+      <div className="flex flex-col w-full">
+        <div className="h-[720px] w-full relative top-0 ">
+          <Image
+            src="/assets/img/snow-mountain.jpg"
+            alt="Hero Section"
+            objectFit="cover"
+            layout="fill"
+            quality={100} />
+          <div className="absolute top-80 left-44 w-full flex gap-10 m-auto container">
+            <input className="px-8 py-3 rounded-full w-[50%] overflow-hidden focus:outline-none focus:shadow-outline focus:border-orange-400 border-2" type="text" placeholder="Search for a job by catagory" />
+            <input className="px-8 py-3 rounded-full w-[50%] overflow-hidden focus:outline-none focus:shadow-outline focus:border-orange-400 border-2 " type="text" placeholder="Search for a job by location" />
+          </div>
+        </div>
         <div>Recent Job posting</div>
         <div>Top categories</div>
         <div>Top Services</div>
