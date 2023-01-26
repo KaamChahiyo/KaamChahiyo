4ca78a538938b2339645578cafdffcceed7fcdea
sumanchalise <sumanchalise57@gmail.com>
2023-01-26 16:32:18 +0545

    category and services added

--- src/components/TopCategory.tsx
+++ src/components/TopCategory.tsx
@@ -1,10 +1,27 @@
 import Link from "next/link";
 import React from "react";
-import { ComputerIcon } from "../icons/ComputerIcon";
-import { ElectricianIcon } from "../icons/ElectricainIcon";
-import { PhotographerIcon } from "../icons/PhotographerIcon";
-import { PlumberIcon } from "../icons/PlumberIcon";
-import { WebdesignerIcon } from "../icons/WebdesignerIcon";
+import {
+  PlumberIcon,
+  ItServiceIcon,
+  PainterIcon,
+  ElectricianIcon,
+  HomeIcon,
+} from "../icons";
+
+export default function TopCategory() {
+  return (
+    <div className=" flex w-full h-32">
+      <div className=" w-1/3 text-center p-5 font-bold ">Top Category</div>
+      <div className="w-2/3 text-sm text-center flex gap-[5%]  ">
+        <Category icon={ElectricianIcon} services={"Electrician"} link="#" />
+        <Category icon={PlumberIcon} services={"Plumber"} link="#" />
+        <Category icon={HomeIcon} services={"House-Hold"} link="#" />
+        <Category icon={ItServiceIcon} services={"IT-Solution"} link="#" />
+        <Category icon={PainterIcon} services={"Painter"} link="#" />
+      </div>
+    </div>
+  );
+}
 
 const Category = ({
   icon,
@@ -17,27 +34,10 @@ const Category = ({
 }) => {
   return (
     <Link href={link}>
-      <div className="flex items-center justify-center rounded-md hover:opacity-60  text-emerald-700 w-24 h-24 ">
-        <div className=" h-20 w-16">
-          {icon}
-          {services}
-        </div>
+      <div className="flex flex-col items-center justify-center rounded-md hover:opacity-60  text-emerald-700 w-24 h-24 ">
+        <div className=" h-20 w-16">{icon}</div>
+        <div> {services}</div>
       </div>
     </Link>
   );
 };
-
-export default function TopCategory() {
-  return (
-    <div className=" flex w-full h-32">
-      <div className=" w-1/3 text-center p-5 font-bold ">Top Category</div>
-      <div className="w-2/3 flex gap-20  ">
-        <Category icon={ElectricianIcon} services={"Electrician"} link="#" />
-        <Category icon={PlumberIcon} services={"Plumber"} link="#" />
-        <Category icon={ComputerIcon} services={"Computer Repair"} link="#" />
-        <Category icon={WebdesignerIcon} services={"Web-Designer"} link="#" />
-        <Category icon={PhotographerIcon} services={"Photo-Grapher"} link="#" />
-      </div>
-    </div>
-  );
-}
--- src/components/TopServices.tsx
+++ src/components/TopServices.tsx
@@ -1,29 +1,43 @@
 import Link from "next/link";
 import React from "react";
-
-const TService = ({ services, link }: { services: string; link: string }) => {
-  return (
-    <Link href={link}>
-      <div className="flex items-center justify-center rounded-md hover:opacity-50 bg-blue-300 text-emerald-700 w-20 h-16 ">
-        <div className=" h-24 w-24">{services}</div>
-      </div>
-    </Link>
-  );
-};
+import {
+  ElectricianIcon,
+  PlumberIcon,
+  ComputerIcon,
+  WebdesignerIcon,
+  PhotographerIcon,
+} from "../icons";
 
 export default function TopServices() {
   return (
-    <div className="flex p-5   h-40 w-full">
-      <div className="p-2 text-center align-center font-bold w-1/3">
-        Top Services
-      </div>
-      <div className="flex w-2/3 gap-20">
-        <TService services={"plumber"} link="#" />
-        <TService services={"plumber"} link="#" />
-        <TService services={"plumber"} link="#" />
-        <TService services={"plumber"} link="#" />
-        <TService services={"plumber"} link="#" />
+    <div className=" flex w-full h-32">
+      <div className=" w-1/3 text-center  p-5 font-bold ">Top Services</div>
+      <div className="w-2/3 text-center  flex text-sm gap-[5%]  ">
+        <Services icon={ElectricianIcon} services={"Electrician"} link="#" />
+        <Services icon={PlumberIcon} services={"Plumber"} link="#" />
+        <Services icon={ComputerIcon} services={"Computer-Repair"} link="#" />
+        <Services icon={WebdesignerIcon} services={"Web-Designer"} link="#" />
+        <Services icon={PhotographerIcon} services={"Photo-Grapher"} link="#" />
       </div>
     </div>
   );
 }
+
+const Services = ({
+  icon,
+  services,
+  link,
+}: {
+  icon: JSX.Element;
+  services: string;
+  link: string;
+}) => {
+  return (
+    <Link href={link}>
+      <div className="flex flex-col items-center justify-center rounded-md hover:opacity-60  text-emerald-700 w-auto h-24 ">
+        <div className=" h-20 w-16">{icon}</div>
+        <div className="font-sm w-auto"> {services}</div>
+      </div>
+    </Link>
+  );
+};
