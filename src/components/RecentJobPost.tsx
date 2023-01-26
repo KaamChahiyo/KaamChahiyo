a652e356a72d95beb382129a7a27c5a7b833b446
Ananta Mishra <mishraananta2017@gmail.com>
2023-01-26 23:13:55 +0545

    minor changes

--- src/components/RecentJobPost.tsx
+++ src/components/RecentJobPost.tsx
@@ -7,11 +7,11 @@ export default function RecentJobPost() {
   let dateTimeOfPost = "5 hours ago";
   let catetegoryOfPost = "Category";
   return (
-    <div className="m-auto">
+    <div className="m-auto container ">
       <div className="p-10 text-center font-bold text-3xl">
         Recent Job posting
       </div>
-      <div className="flex gap-5  px-10 py-10 mx-5 my-5">
+      <div className="flex gap-5  px-10 py-10 mx-5 my-5 ">
         <div className="Job_1 w-1/3 shadow-lg hover:shadow-sm">
           <div className="font-bold p-5 text-xl">Job Title</div>
           <div className="flex gap-4 italic p-3">
