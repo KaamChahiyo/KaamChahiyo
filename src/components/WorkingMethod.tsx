9e2847e6696134ac2439b5463a4673465e00121c
sumanchalise <sumanchalise57@gmail.com>
2023-01-26 23:07:32 +0545

    Working Method Added

--- src/components/WorkingMethod.tsx
+++ src/components/WorkingMethod.tsx
@@ -14,13 +14,13 @@ export default function WorkingMethod() {
         />
         <Method
           step="2"
-          name={"Post Job/Apply for Job "}
+          name={"Post / Apply for Job "}
           description={"Post Job or Apply for Job as per your need."}
           link="#"
         />
         <Method
           step="3"
-          name={"Get Paid"}
+          name={"Get Pay/Paid"}
           description={"After Job is Completed Get Paid "}
           link="#"
         />
