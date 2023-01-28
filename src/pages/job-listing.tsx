7d6c2e620c259716395062c442cf864c757953a9
acdepak <acdepak@gmail.com>
2023-01-28 14:32:34 +0545

    addition of tags & userImage

--- src/pages/job-listing.tsx
+++ src/pages/job-listing.tsx
@@ -1,17 +1,76 @@
+import Image from "next/image";
 import React, { useState } from "react";
 
 export default function JobListing() {
+  let jobTags;
   return (
-    <div className="flex flex-col items-center justify-center p-10 bg-gradient-to-b from-emerald-50 to-emerald-100">
+    <div className="flex flex-col items-center justify-center p-10 bg-gradient-to-b from-white to-emerald-50">
       <div className="flex font-bold text-2xl p-3">Jobs you might like</div>
       <div className="flex flex-col items-center justify-center gap-4">
         <JobList
           postBy="Ramesh"
-          postByPic=""
+          postByPicURL="/assets/img/profile-image.png"
           dateTimeOfPost="5 hours ago"
           catetegoryOfPost="Plumber"
           locationOfPost="Bharatpur"
-          jobTitleOfPost="Job Title"
+          jobTitleOfPost="Water Leakage & Tap Installation"
+          jobTagsOfPost={
+            (jobTags = [
+              { tag: "Leakage" },
+              { tag: "Tap Installation" },
+              { tag: "Plumbing" },
+            ])
+          }
+          jobDescriptionOfPost="Description of the job is wirtten here. We should write what
+        problem or solutions we need on this section. Employer posts a job
+        and this is description from that post. Description of the job is wirtten here. We should write what
+        problem or solutions we need on this section. Employer posts a job
+        and this is description from that post.Description of the job is wirtten here. We should write what
+        problem or solutions we need on this section. Employer posts a job
+        and this is description from that post.Description of the job is wirtten here. We should write what
+        problem or solutions we need on this section. Employer posts a job
+        and this is description from that post."
+        />
+
+        <JobList
+          postBy="Ramesh"
+          postByPicURL="/assets/img/profile-image.png"
+          dateTimeOfPost="5 hours ago"
+          catetegoryOfPost="Plumber"
+          locationOfPost="Bharatpur"
+          jobTitleOfPost="Water Leakage & Tap Installation"
+          jobTagsOfPost={
+            (jobTags = [
+              { tag: "Leakage" },
+              { tag: "Tap Installation" },
+              { tag: "Plumbing" },
+            ])
+          }
+          jobDescriptionOfPost="Description of the job is wirtten here. We should write what
+        problem or solutions we need on this section. Employer posts a job
+        and this is description from that post. Description of the job is wirtten here. We should write what
+        problem or solutions we need on this section. Employer posts a job
+        and this is description from that post.Description of the job is wirtten here. We should write what
+        problem or solutions we need on this section. Employer posts a job
+        and this is description from that post.Description of the job is wirtten here. We should write what
+        problem or solutions we need on this section. Employer posts a job
+        and this is description from that post."
+        />
+
+        <JobList
+          postBy="Ramesh"
+          postByPicURL="/assets/img/profile-image.png"
+          dateTimeOfPost="5 hours ago"
+          catetegoryOfPost="Plumber"
+          locationOfPost="Bharatpur"
+          jobTitleOfPost="Water Leakage & Tap Installation"
+          jobTagsOfPost={
+            (jobTags = [
+              { tag: "Leakage" },
+              { tag: "Tap Installation" },
+              { tag: "Plumbing" },
+            ])
+          }
           jobDescriptionOfPost="Description of the job is wirtten here. We should write what
         problem or solutions we need on this section. Employer posts a job
         and this is description from that post. Description of the job is wirtten here. We should write what
@@ -33,28 +92,39 @@ const JobList = ({
   locationOfPost,
   jobDescriptionOfPost,
   postBy,
-  postByPic,
+  postByPicURL,
   jobTitleOfPost,
+  jobTagsOfPost,
 }: {
   dateTimeOfPost: string;
   catetegoryOfPost: string;
   locationOfPost: string;
   jobDescriptionOfPost: string;
   postBy: string;
-  postByPic: string;
+  postByPicURL: string;
   jobTitleOfPost: string;
+  jobTagsOfPost: any;
 }) => {
   return (
     <div className="w-3/5 shadow-lg shadow-green-200 hover:shadow-sm p-3">
       <div className="font-bold text-xl p-2">{jobTitleOfPost}</div>
       <div className="flex gap-4 italic p-3">
+        <div className="h-7 w-7 relative rounded-full overflow-hidden ">
+          <Image src={postByPicURL} alt="Profile Image" fill />
+        </div>
         <p>{postBy}</p>
-        <p>{postByPic}</p>
         <p>{dateTimeOfPost}</p>
         <p>{catetegoryOfPost}</p>
       </div>
       <div className="jobDetail text-lg px-3">
         <ReadMoreBtn>{jobDescriptionOfPost}</ReadMoreBtn>
+      </div>
+      <div className="flex mx-10 ">
+        <div className="flex gap-2 ">
+          {jobTagsOfPost.map((item: { tag: string }) => (
+            <div className="rounded-2xl bg-slate-100 p-2">{item.tag}</div>
+          ))}
+        </div>
       </div>
       <div className="italic p-8">{locationOfPost}</div>
     </div>
@@ -71,7 +141,7 @@ const ReadMoreBtn = ({ children }: { children: any }) => {
     <div>
       {isReadMoreShown ? children : children.substring(0, 200)}
       <button onClick={toggleBtn} className="font-bold text-teal-600 px-2">
-        {isReadMoreShown ? " Less " : " ...More "}
+        {isReadMoreShown ? " Show less " : " ...Show more "}
       </button>
     </div>
   );
