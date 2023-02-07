4a112e51d129dffb5f48e442f18ad3249b5da0a1
acdepak <acdepak@gmail.com>
2023-02-07 19:38:12 +0545

    layout updated

--- src/pages/userProfile.tsx
+++ src/pages/userProfile.tsx
@@ -5,6 +5,7 @@ import { useRouter } from "next/router";
 import { TabPanel, useTabs } from "react-headless-tabs";
 import { TabSelector } from "../components/TabSelector";
 import { HomeIcon } from "../icons";
+import { useState } from "react";
 
 
 export default function Profile() {
@@ -19,14 +20,23 @@ export default function Profile() {
         router.replace("/login")
     }
     else {
-        // const [userEmail, setUserEmail] = useState(session.user.email);
-        // const emailChange = (e) => { setUserEmail(e.target.value) };
+        // const TabOpen = () => {
+        const [userEmail, setUserEmail] = useState(session.user.email);
+        const emailChange = (e) => { setUserEmail(e.target.value) };
 
-        // const [userName, setUserName] = useState(session.user.name);
-        // const nameChange = (e) => { setUserName(e.target.value) };
+        const [userName, setUserName] = useState(session.user.name);
+        const nameChange = (e) => { setUserName(e.target.value) };
 
-        // const [userPassword, setUserPassword] = useState(session.user.password);
-        // const passwordChange = (e) => { setUserPassword(e.target.value) };
+        const [userBio, setUserBio] = useState("");
+        const bioChange = (e) => {
+            setUserBio(e.target.value);
+
+            // const [userPassword, setUserPassword] = useState("");
+            // const passwordChange = (e) => { setUserPassword(e.target.value) };
+            // return (
+
+            // );
+        }
 
 
         return (
@@ -62,61 +72,85 @@ export default function Profile() {
                             </TabSelector>
 
                         </div>
+                        {/* <TabOpen /> */}
                         <div className="w-3/4 ">
                             <TabPanel hidden={selectedTab !== "profile-tab"}>
-                                <div className='flex flex-col gap-3 p-10 bg-white shadow-md rounded-3xl '>
-                                    <div className="flex flex-col gap-3">
-                                        <p className="font-bold text-4xl text-center p-4">Update your profile</p>
-                                    </div>
-                                    {/* <div className='flex flex-col'>
-                                        <div className=''>
-                                            <Image src={session?.user?.image} alt={session?.user?.name} width={100} height={100} quality={100} />
+                                <div className="p-10">
+                                    <div className='flex flex-col gap-3 p-10 bg-white shadow-md rounded-3xl h-[672px]'>
+                                        <div className="flex flex-col gap-3">
+                                            <p className="font-bold text-4xl text-center p-4">Update your profile</p>
                                         </div>
-                                        <div>{session.user.name}</div>
-                                    </div> */}
-                                    {/* <div className='flex flex-col gap-1 text-gray-500'>
-                                        <label className='text-grey'>Name:</label>
-                                        <input type="text" placeholder="Name" value={userName} onChange={nameChange}
-                                            className='border-2 focus:outline-none focus:shadow-outline border-gray-300 p-3 rounded-md'
-                                        />
+                                        <div className='flex flex-col'>
+                                            <div className=''>
+                                                <Image src={session?.user?.image} alt={session?.user?.name} width={100} height={100} quality={100} />
+                                            </div>
+                                            <div>{session.user.name}</div>
+                                        </div>
+                                        <div className='flex flex-col gap-1 text-gray-500'>
+                                            <label className='text-grey'>Name:</label>
+                                            <input type="text" placeholder="Name" value={userName} onChange={nameChange}
+                                                className='border-2 focus:outline-none focus:shadow-outline border-gray-300 p-3 rounded-md'
+                                            />
 
-                                    </div> */}
-                                    {/* <div className='flex flex-col gap-1 text-gray-500'>
-                                        <label>Email:</label>
-                                        <input type="text" placeholder="email" value={userEmail} onChange={emailChange}
-                                            className='border-2 focus:outline-none focus:shadow-outline border-gray-300 p-3 rounded-md'
-                                        />
-                                    </div> */}
-                                    {/* <div className='flex flex-col gap-1 text-gray-500'>
-                                        <label>Bio:</label>
-                                        <textarea rows={3} value={userBio} onChange={bioChange}
-                                            className='border-2 focus:outline-none focus:shadow-outline px-3 py-3 border-gray-300 text-gray-700 leading-tight w-full rounded-md'
-                                        />
-                                    </div> */}
-                                    <Link href="">
-                                        <div>
-                                            <button className="px-5 py-4 bg-teal-900 hover:bg-teal-700 rounded-lg text-white text-xl font-bold sm:w-full focus:outline-none focus:shadow-outline">Update</button>
                                         </div>
-                                    </Link>
+                                        <div className='flex flex-col gap-1 text-gray-500'>
+                                            <label>Email:</label>
+                                            <input type="text" placeholder="email" value={userEmail} onChange={emailChange}
+                                                className='border-2 focus:outline-none focus:shadow-outline border-gray-300 p-3 rounded-md'
+                                            />
+                                        </div>
+                                        <div className='flex flex-col gap-1 text-gray-500'>
+                                            <label>Bio:</label>
+                                            <textarea rows={3} value={userBio} onChange={bioChange}
+                                                className='border-2 focus:outline-none focus:shadow-outline px-3 py-3 border-gray-300 text-gray-700 leading-tight w-full rounded-md'
+                                            />
+                                        </div>
+                                        <Link href="">
+                                            <div>
+                                                <button className="px-5 py-4 bg-teal-900 hover:bg-teal-700 rounded-lg text-white text-xl font-bold sm:w-full focus:outline-none focus:shadow-outline">Update</button>
+                                            </div>
+                                        </Link>
+                                    </div>
                                 </div>
                             </TabPanel>
                             <TabPanel hidden={selectedTab !== "security-tab"}>
-                                <div className='flex flex-col gap-3 p-10 bg-white shadow-md rounded-3xl h-[813px]'>
-                                    <div className="flex flex-col gap-3">
-                                        <p className="font-bold text-4xl text-center p-4">Update your Security</p>
-                                    </div>
-
-                                    <div className='flex flex-col gap-1 text-gray-500'>
-                                        <label>Password:</label>
-                                        <input type="password" placeholder="password" value=""
-                                            className='border-2 focus:outline-none border-gray-300 p-3 rounded-md'
-                                        />
-                                    </div>
-                                    <Link href="">
-                                        <div>
-                                            <button className="px-5 py-4 bg-teal-900 hover:bg-teal-700 rounded-lg text-white text-xl font-bold sm:w-full focus:outline-none focus:shadow-outline">Update</button>
+                                <div className="p-10">
+                                    <div className='flex flex-col gap-3 p-10 bg-white shadow-md rounded-3xl h-[672px]'>
+                                        <div className="flex flex-col gap-3">
+                                            <p className="font-bold text-4xl text-center p-4">Change your password</p>
                                         </div>
-                                    </Link>
+
+                                        <div className='flex flex-col gap-1 text-gray-500'>
+                                            <label>Current Password:</label>
+                                            <input type="password" placeholder="Enter current Password" value=""
+                                                className='border-2 focus:outline-none border-gray-300 p-3 rounded-md'
+                                            />
+                                        </div>
+                                        <div className='flex flex-col gap-1 text-gray-500'>
+                                            <label>New password:</label>
+                                            <input type="password" placeholder="Type new password" value=""
+                                                className='border-2 focus:outline-none border-gray-300 p-3 rounded-md'
+                                            />
+                                        </div>
+                                        <div className='flex flex-col gap-1 text-gray-500'>
+                                            <label>Retype new password:</label>
+                                            <input type="password" placeholder="Re-type password" value=""
+                                                className='border-2 focus:outline-none border-gray-300 p-3 rounded-md'
+                                            />
+                                        </div>
+                                        <div className="relative h-full ">
+                                            <div className="absolute top-11 right-0 hover:cursor-text p-5">
+                                                <button className="px-4 py-2 bg-teal-900 hover:bg-teal-700 rounded-lg text-white text-xl items-center">
+                                                    Forgot Password</button>
+                                            </div>
+
+
+                                            <div className="absolute inset-x-0 bottom-0">
+                                                <button className="px-5 py-4 bg-teal-900 hover:bg-teal-700 rounded-lg text-white text-xl font-bold sm:w-full focus:outline-none focus:shadow-outline">Update</button>
+                                            </div>
+                                        </div>
+
+                                    </div>
                                 </div>
                             </TabPanel>
                         </div>
@@ -127,3 +161,4 @@ export default function Profile() {
         );
     };
 }
+//}
