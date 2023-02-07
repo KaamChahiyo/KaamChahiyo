525ad50f3ab56c4228a3bc1e573ffe44f1718687
Ananta Mishra <mishraananta2017@gmail.com>
2023-02-07 12:48:45 +0545

    updated: color schema

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -4,7 +4,7 @@ import { FacebookIcon, GithubIcon, TelegramIcon } from "../icons";
 const FooterIcon = ({ icon, link }: { icon: JSX.Element; link: string }) => {
   return (
     <Link href={link}>
-      <div className="flex items-center justify-center rounded-full hover:opacity-50 bg-white text-emerald-700 w-10 h-10 ">
+      <div className="flex items-center justify-center rounded-full hover:opacity-50 bg-white text-[#0063F1] w-10 h-10 ">
         <div className="h-8 w-8 ">{icon}</div>
       </div>
     </Link>
