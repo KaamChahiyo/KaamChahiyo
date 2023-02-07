b8388f6207057db45c642237e4f0096e8b654a0f
acdepak <acdepak@gmail.com>
2023-02-07 14:48:53 +0545

    updated: included tabs

--- src/pages/userProfile.tsx
+++ src/pages/userProfile.tsx
@@ -14,73 +14,94 @@ export default function UserProfile() {
         router.replace("/login")
     }
     else {
-        const [userEmail, setUserEmail] = useState(session.user.email);
-        const emailChange = (e) => { setUserEmail(e.target.value) };
+        const TabOpen = () => {
+            const [userEmail, setUserEmail] = useState(session.user.email);
+            const emailChange = (e) => { setUserEmail(e.target.value) };
 
-        const [userName, setUserName] = useState(session.user.name);
-        const nameChange = (e) => { setUserName(e.target.value) };
+            const [userName, setUserName] = useState(session.user.name);
+            const nameChange = (e) => { setUserName(e.target.value) };
 
-        const [userBio, setUserBio] = useState(session.user.bio);
-        const bioChange = (e) => { setUserBio(e.target.value) };
+            const [userBio, setUserBio] = useState(session.user.bio);
+            const bioChange = (e) => { setUserBio(e.target.value) };
 
-        // const [userPassword, setUserPassword] = useState(session.user.password);
-        // const passwordChange = (e) => { setUserPassword(e.target.value) };
+            const [userPassword, setUserPassword] = useState("");
+            const passwordChange = (e) => { setUserPassword(e.target.value) };
+            return (
+                <div>
+                    {showProfile ?
+                        <div className='flex flex-col gap-3 p-10 bg-white shadow-md rounded-3xl '>
+                            <div className="flex flex-col gap-3">
+                                <p className="font-bold text-4xl text-center p-4">Update your profile</p>
+                            </div>
+                            <div className='flex flex-col'>
+                                <div className=''>
+                                    <Image src={session?.user?.image} alt={session?.user?.name} width={100} height={100} quality={100} />
+                                </div>
+                                <div>{session.user.name}</div>
+                            </div>
+                            <div className='flex flex-col gap-1 text-gray-500'>
+                                <label className='text-grey'>Name:</label>
+                                <input type="text" placeholder="Name" value={userName} onChange={nameChange}
+                                    className='border-2 focus:outline-none focus:shadow-outline border-gray-300 p-3 rounded-md'
+                                />
+
+                            </div>
+                            <div className='flex flex-col gap-1 text-gray-500'>
+                                <label>Email:</label>
+                                <input type="text" placeholder="email" value={userEmail} onChange={emailChange}
+                                    className='border-2 focus:outline-none focus:shadow-outline border-gray-300 p-3 rounded-md'
+                                />
+                            </div>
+                            <div className='flex flex-col gap-1 text-gray-500'>
+                                <label>Bio:</label>
+                                <textarea rows={3} value={userBio} onChange={bioChange}
+                                    className='border-2 focus:outline-none focus:shadow-outline px-3 py-3 border-gray-300 text-gray-700 leading-tight w-full rounded-md'
+                                />
+                            </div>
+                            <Link href="">
+                                <div>
+                                    <button className="px-5 py-4 bg-teal-900 hover:bg-teal-700 rounded-lg text-white text-xl font-bold sm:w-full focus:outline-none focus:shadow-outline">Update</button>
+                                </div>
+                            </Link>
+                        </div>
+                        :
+                        <div className='flex flex-col gap-3 p-10 bg-white shadow-md rounded-3xl h-[813px]'>
+                            <div className="flex flex-col gap-3">
+                                <p className="font-bold text-4xl text-center p-4">Update your Security</p>
+                            </div>
+
+                            <div className='flex flex-col gap-1 text-gray-500'>
+                                <label>Password:</label>
+                                <input type="password" placeholder="password" value={userPassword} onChange={passwordChange}
+                                    className='border-2 focus:outline-none border-gray-300 p-3 rounded-md'
+                                />
+                            </div>
+                            <Link href="">
+                                <div>
+                                    <button className="px-5 py-4 bg-teal-900 hover:bg-teal-700 rounded-lg text-white text-xl font-bold sm:w-full focus:outline-none focus:shadow-outline">Update</button>
+                                </div>
+                            </Link>
+                        </div>
+                    }
+                </div>
+            );
+        }
+
+        const [showProfile, showSecurity] = useState(true);
+        const ToggleProfile = () => { showSecurity((prevState) => !prevState) };
+
         return (
-            <div className='flex justify-center items-center my-5 gap-5'>
-
-                <div className='flex flex-col bg-white shadow-md rounded-3xl gap-3 p-5 w-1/3'>
-                    <div>
-                        <Link href="#">
-                            <div className='flex flex-col text-lg items-center hover:scale-110'>Profile</div>
-                        </Link>
+            <div className='flex justify-center items-center my-5 gap-5 '>
+                <div className='flex flex-col justify-center bg-slate-300 shadow-md rounded-3xl w-1/3 h-40'>
+                    <div className='bg-white p-6' onClick={ToggleProfile}>
+                        <div className='flex flex-col text-lg items-center hover:scale-110 '
+                        >Profile</div>
                     </div>
-                    <div>
-                        <Link href="#">
-                            <div className='flex flex-col text-lg items-center hover:scale-110'>Security</div>
-                        </Link>
+                    <div className='bg-white p-6' onClick={ToggleProfile}>
+                        <div className='flex flex-col text-lg items-center hover:scale-110'>Security</div>
                     </div>
                 </div>
-                <div className='flex flex-col gap-3 p-10 bg-white w-1/3 shadow-md rounded-3xl'>
-                    <div className="flex flex-col gap-3">
-                        <p className="font-bold text-4xl text-center p-4">Update your profile</p>
-                    </div>
-                    <div className='flex flex-col'>
-                        <div className=''>
-                            <Image src={session?.user?.image} alt={session?.user?.name} width={100} height={100} quality={100} />
-                        </div>
-                        <div>{session.user.name}</div>
-                    </div>
-                    <div className='flex flex-col gap-1 text-gray-500'>
-                        <label className='text-grey'>Name:</label>
-                        <input type="text" placeholder="Name" value={userName} onChange={nameChange}
-                            className='border-2 focus:outline-none focus:shadow-outline border-gray-300 p-3 rounded-md'
-                        />
-
-                    </div>
-                    <div className='flex flex-col gap-1 text-gray-500'>
-                        <label>Email:</label>
-                        <input type="text" placeholder="email" value={userEmail} onChange={emailChange}
-                            className='border-2 focus:outline-none focus:shadow-outline border-gray-300 p-3 rounded-md'
-                        />
-                    </div>
-                    <div className='flex flex-col gap-1 text-gray-500'>
-                        <label>Bio:</label>
-                        <textarea rows={3} value={userBio} onChange={bioChange}
-                            className='border-2 focus:outline-none focus:shadow-outline px-3 py-3 border-gray-300 text-gray-700 leading-tight w-full rounded-md'
-                        />
-                    </div>
-                    {/* <div className='flex flex-col gap-1 text-gray-500'>
-                        <label>Password:</label>
-                        <input type="password" placeholder="password" value={userPassword} onChange={passwordChange}
-                            className='border-2 focus:outline-none border-gray-300 p-3 rounded-md'
-                        />
-                    </div> */}
-                    <Link href="">
-                        <div>
-                            <button className="px-5 py-4 bg-teal-900 hover:bg-teal-700 rounded-lg text-white text-xl font-bold sm:w-full focus:outline-none focus:shadow-outline">Update</button>
-                        </div>
-                    </Link>
-                </div>
+                <TabOpen />
             </div >
         );
     }
