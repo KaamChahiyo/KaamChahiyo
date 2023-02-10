ffed34c7e5f1303cafe16f86ca7374b5fdc1983f
Arun Kumar Pariyar <openarungeek@gmail.com>
2023-02-10 13:39:54 +0545

    fix UI hydrating error and set session check on useEffect

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from "next/router";
 import { useSnackbar } from 'notistack';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useForm } from "react-hook-form";
 import { authOptions } from './api/auth/[...nextauth]';
 
@@ -42,65 +42,65 @@ export default function Login() {
         }
     }
 
-    if (typeof window === "undefined") return null
+    useEffect(() => {
+        if (session) {
+            router.replace("/")
+        }
+    }, [session])
 
-    if (session) {
-        router.replace("/")
-    }
-    else
-        return (
-            <>
-                <div className="flex justify-center items-center my-36">
-                    <div className="flex flex-col w-1/4 relative justify-center gap-6 p-10 bg-white rounded-3xl md:shadow-[0_3px_25px_-10px_rgba(0,0,0,0.3)] ">
-                        <form onSubmit={handleSubmit(onSubmit)} >
-                            <div className=" flex flex-col gap-5 ">
-                                <div className='flex flex-col gap-3'>
-                                    <p className=" font-bold text-4xl text-center "> Login</p>
-                                    <p className=" text-gray-400 text-center text-md ">Enter your detail to login to your account</p>
-                                </div>
-                                <div className='flex flex-col gap-3 '>
-                                    <div className='flex flex-col gap-1.5'>
-                                        <label className=" text-gray-500 font-semibold text-sm uppercase  tracking-[2.78px] ">Email</label>
-                                        <input {...register("email")} type="email" placeholder="Email" className="border-2 focus:outline-none focus:shadow-outline px-3 py-3 border-gray-300 text-gray-700 leading-tight w-full rounded-md" />
-                                    </div>
-                                    <div className='flex flex-col gap-1.5'>
-                                        <label className=" text-gray-500 font-semibold text-sm uppercase  tracking-[2.78px] ">Password</label>
-                                        <input {...register("password")} type="password" placeholder="Password" className="border-2 focus:outline-none focus:shadow-outline px-3 py-3 border-gray-300 text-gray-700 leading-tight w-full rounded-md" />
-                                    </div>
-                                    <button type='submit' onClick={() => signIn("credentials")} className="px-9 py-4 bg-teal-900 hover:bg-oteal-900 rounded-lg text-white text-xl font-bold sm:w-full focus:outline-none focus:shadow-outline">
-                                        {isSubmitting ? <>Signing In</> : <>Sign In</>}
-                                    </button>
-                                </div>
+    return (
+        <>
+            <div className="flex justify-center items-center my-36">
+                <div className="flex flex-col w-1/4 relative justify-center gap-6 p-10 bg-white rounded-3xl md:shadow-[0_3px_25px_-10px_rgba(0,0,0,0.3)] ">
+                    <form onSubmit={handleSubmit(onSubmit)} >
+                        <div className=" flex flex-col gap-5 ">
+                            <div className='flex flex-col gap-3'>
+                                <p className=" font-bold text-4xl text-center "> Login</p>
+                                <p className=" text-gray-400 text-center text-md ">Enter your detail to login to your account</p>
                             </div>
-                        </form>
-                        <div>
-                            <p className="font-medium text-sm text-[#9A9A9A] text-center ">
-                                --- Or login with ---
-                            </p>
-
-                            <div className='flex gap-6 flex-col w-full'>
-                                <button onClick={() => signIn("facebook")} className='border border-gray-200 rounded-md flex gap-6 px-6 py-3 w-full items-center'>
-                                    <div className='w-6 relative'>
-                                        <Image alt="" src="/assets/img/FacebookIcon.svg" width={20} height={20} />
-                                    </div>
-                                    <div className='flex flex-1 items-center justify-center'>Continue with Facebook</div>
-                                </button>
-                                <button onClick={() => signIn("google")} className='border border-gray-200 rounded-md flex gap-6 px-6 py-3 w-full items-center'>
-                                    <div className='w-6 relative'>
-                                        <Image alt="" src="/assets/img/GoogleIcon.svg" width={20} height={20} />
-                                    </div>
-                                    <div className='flex flex-1 items-center justify-center'>Continue with Google</div>
-                                </button>
-                                <div>
-                                    <p className="text-center ">Don't have account ?
-                                    </p>
-
-                                    <p className="text-teal-900 font-bold text-sm text-center">
-                                        <Link href="/signup">Signup</Link>
-                                    </p>
+                            <div className='flex flex-col gap-3 '>
+                                <div className='flex flex-col gap-1.5'>
+                                    <label className=" text-gray-500 font-semibold text-sm uppercase  tracking-[2.78px] ">Email</label>
+                                    <input {...register("email")} type="email" placeholder="Email" className="border-2 focus:outline-none focus:shadow-outline px-3 py-3 border-gray-300 text-gray-700 leading-tight w-full rounded-md" />
                                 </div>
+                                <div className='flex flex-col gap-1.5'>
+                                    <label className=" text-gray-500 font-semibold text-sm uppercase  tracking-[2.78px] ">Password</label>
+                                    <input {...register("password")} type="password" placeholder="Password" className="border-2 focus:outline-none focus:shadow-outline px-3 py-3 border-gray-300 text-gray-700 leading-tight w-full rounded-md" />
+                                </div>
+                                <button type='submit' onClick={() => signIn("credentials")} className="px-9 py-4 bg-teal-900 hover:bg-oteal-900 rounded-lg text-white text-xl font-bold sm:w-full focus:outline-none focus:shadow-outline">
+                                    {isSubmitting ? <>Signing In</> : <>Sign In</>}
+                                </button>
                             </div>
-                            {/* <div className="flex p-5 justify-center gap-5 flex-col ">
+                        </div>
+                    </form>
+                    <div>
+                        <p className="font-medium text-sm text-[#9A9A9A] text-center ">
+                            --- Or login with ---
+                        </p>
+
+                        <div className='flex gap-6 flex-col w-full'>
+                            <button onClick={() => signIn("facebook")} className='border border-gray-200 rounded-md flex gap-6 px-6 py-3 w-full items-center'>
+                                <div className='w-6 relative'>
+                                    <Image alt="" src="/assets/img/FacebookIcon.svg" width={20} height={20} />
+                                </div>
+                                <div className='flex flex-1 items-center justify-center'>Continue with Facebook</div>
+                            </button>
+                            <button onClick={() => signIn("google")} className='border border-gray-200 rounded-md flex gap-6 px-6 py-3 w-full items-center'>
+                                <div className='w-6 relative'>
+                                    <Image alt="" src="/assets/img/GoogleIcon.svg" width={20} height={20} />
+                                </div>
+                                <div className='flex flex-1 items-center justify-center'>Continue with Google</div>
+                            </button>
+                            <div>
+                                <p className="text-center ">Don't have account ?
+                                </p>
+
+                                <p className="text-teal-900 font-bold text-sm text-center">
+                                    <Link href="/signup">Signup</Link>
+                                </p>
+                            </div>
+                        </div>
+                        {/* <div className="flex p-5 justify-center gap-5 flex-col ">
                             <Link href="#">
                                 <div className="bg-teal-100 flex justify-center p-3 rounded-full" >
                                     <Image alt="" src="/assets/img/FacebookIcon.svg" width={20} height={20} />
@@ -120,11 +120,11 @@ export default function Login() {
                                 </p>
                             </div>
                         </div> */}
-                        </div>
-                    </div >
-                </div>
-            </>
-        )
+                    </div>
+                </div >
+            </div>
+        </>
+    )
 }
 
 
@@ -138,4 +138,4 @@ export async function getServerSideProps(context) {
             ),
         },
     }
-}       
+}
