f5f94ef8abf69c1f43186abb29a97d1d403b3a2e
acdepak <acdepak@gmail.com>
2023-02-12 21:01:49 +0545

    updated: design and props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 
-export default function Button() {
+export default function Button({value, onClick}:any) {
   return (
     <div>
-      <input type="button" value="" className="bg-blue-100" />
+        <input
+          type="button"
+          value={value}          
+          className="flex rounded-full bg-[#0063F1] text-white text-lg font-medium px-8 py-2.5 border-2 border-[#0063F1]
+          hover:bg-white hover:font-medium hover:text-[#0063F1] hover:border-2"
+          onClick={onClick}
+        />
     </div>
   );
 }
