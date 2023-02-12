7cdd6ea24870d6e9e32bc307e7bd0b4f5647a89c
acdepak <acdepak@gmail.com>
2023-02-13 00:47:15 +0545

    added: condition for css

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,21 @@
 import React from "react";
 
-export default function Button({value, onClick}:{value:React.ReactNode; onClick:any;}) {
+export default function Button({value, onClick}:any) {
+  // let className ="flex rounded-md bg-[#0063F1] text-white text-lg font-medium px-6 py-2.5 border-2 border-[#0063F1] hover:bg-white hover:font-medium hover:text-[#0063F1] hover:border-2";
+  // if(value == "Search") {
+  //   className="flex rounded-full bg-[#0063F1] text-white text-lg font-medium px-6 py-2.5 border-2 border-[#0063F1] hover:bg-white hover:font-medium hover:text-[#0063F1] hover:border-2"
+  // }
   return (
     <div>
-        <button className={`flex rounded-full bg-[#0063F1] text-white text-lg font-medium px-8 py-2.5 border-2 border-[#0063F1] hover:bg-white hover:font-medium hover:text-[#0063F1] hover:border-2 `}
+        <input
+          type="button"
+          value={value}          
+          //className={className}
+          className={value !== "Search" ? 
+            "flex rounded-md bg-[#0063F1] text-white text-lg font-medium px-6 py-2.5 border-2 border-[#0063F1] hover:bg-white hover:font-medium hover:text-[#0063F1] hover:border-2" :
+            "flex rounded-full bg-[#0063F1] text-white text-lg font-medium px-6 py-2.5 border-2 border-[#0063F1] hover:bg-white hover:font-medium hover:text-[#0063F1] hover:border-2"}
           onClick={onClick}
-        >{value}
-        </button>
+        />
     </div>
   );
 }
