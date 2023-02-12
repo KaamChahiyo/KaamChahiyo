59790eac40931b55d7e90d80076ec4097ac6e79c
sumanchalise <sumanchalise57@gmail.com>
2023-02-12 13:21:06 +0545

    date-fns added for days count

--- src/pages/apply-job.tsx
+++ src/pages/apply-job.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { formatDistance } from "date-fns";
+import Image from "next/image";
 
 export default function jobApply({ jobs }) {
   return (
@@ -10,17 +12,27 @@ export default function jobApply({ jobs }) {
               <div className="shadow-md shadow-green-200 rounded-lg hover:shadow-sm p-3">
                 <div className="font-bold text-xl p-2">{job.title}</div>
                 <div className="flex gap-4 italic p-3">
-                  <p>{job.image}</p>
-                  <p>{job.postedBy.name}</p>
-                  <p>{job.postedOn}</p>
-                  <p>{job.category}</p>
+                  <div>
+                    <Image src="" alt="" />
+                  </div>
+                  <div>{job.postedBy.name}</div>
+                  <div className="bg-blue-50 rounded-full px-3 ">
+                    {formatDistance(new Date(job.postedOn), new Date())} ago
+                  </div>
+                  <div className="bg-blue-50 rounded-full px-3 ">
+                    {job.Category.displayName}
+                  </div>
                 </div>
                 <div className="jobDetail text-lg px-3 w-full">
                   {job.description}
                 </div>
+                <div className="flex pt-5 px-3">
+                  <span className="font-semibold">Price:</span> &#160;
+                  {job.price}
+                </div>
 
-                <div className="flex flex-col  p-5">
-                  <div className="italic p-2">{job.Location.displayName}</div>
+                <div className="flex flex-col pb-3">
+                  <div className="italic p-3">{job.Location.displayName}</div>
 
                   <div className="p-2 bg-emerald-500 hover:bg-emerald-400 text-lg font-bold w-fit rounded-md">
                     <button className="px-2 py-1 rounded-2xl text-white">
