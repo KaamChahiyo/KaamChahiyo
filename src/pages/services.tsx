d9f1b413b5f3b5d150cba884f673bdc4caf6c7a7
acdepak <acdepak@gmail.com>
2023-02-22 01:28:02 +0545

    made responsive

--- src/pages/services.tsx
+++ src/pages/services.tsx
@@ -13,8 +13,8 @@ import {
 
 export default function Services() {
   return (
-    <div className="flex items-center justify-center pb-5 m-auto">
-      <div className="grid grid-cols-3 p-3 gap-12 place-content-center">
+    <div className="flex justify-center">
+      <div className="grid grid-cols-1 sm:grid-cols-3 py-5 gap-12 place-content-center">
         <AllServices
           link=""
           icon={PlumberIcon}
@@ -92,7 +92,7 @@ const AllServices = ({
 }) => {
   return (
     <Link passHref href={link} className="flex gap-10">
-      <div className="flex flex-row w-auto gap-5 shadow border border-gray-200 hover:border-cyan-600  rounded-lg overflow-hidden p-5 bg-slate-50 hover:scale-110  items-center">
+      <div className="flex items-center gap-5 shadow border border-gray-200 hover:border-cyan-600 rounded-lg overflow-hidden p-5 bg-slate-50 hover:scale-110 ">
         <div className="w-24 opacity-70 ">{icon}</div>
         <div className="flex flex-col">
           <div className="font-bold text-3xl w-48 pb-1">{services}</div>
