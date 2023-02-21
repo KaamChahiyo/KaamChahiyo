01ba722a1b7a476d424738135b0d535ea48d9612
acdepak <acdepak@gmail.com>
2023-02-22 01:58:34 +0545

    fixed:css, made responsive

--- src/components/RecentJobPost.tsx
+++ src/components/RecentJobPost.tsx
@@ -24,10 +24,10 @@ export default function RecentJobPost() {
   return (
     <div className="flex flex-col justify-center items-center gap-5">
       <div className="flex font-bold text-2xl py-4">Recent Jobs</div>
-      <div className="flex flex-col items-center justify-center gap-4">
+      <div className="flex flex-col items-center gap-4">
         {job.slice(0, 2).map((job) => {
           return (
-            <div key={job.id} className="p-3 w-3/5">
+            <div key={job.id} className="w-full sm:w-3/5 p-1">
               <div className="shadow border border-gray-200  hover:border-cyan-600  rounded-lg overflow-hidden p-3">
                 <div className="font-bold text-xl p-2">{job.title}</div>
                 <div className="flex gap-4 italic p-3 m-auto items-center">
