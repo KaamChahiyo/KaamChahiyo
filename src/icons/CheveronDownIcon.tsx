0cc77e317ccafa4dbde4e84fb887a04edca34f9f
Ananta Mishra <mishraananta2017@gmail.com>
2023-03-04 16:48:14 +0545

    searchbar updated with cheverondown icon

---
+++ src/icons/CheveronDownIcon.tsx
@@ -0,0 +1,4 @@
+export const ChevronDownIcon =
+    <svg fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M6 9L12 15L18 9" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+    </svg>
