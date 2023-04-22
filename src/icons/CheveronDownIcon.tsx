6feaf6414a532b7ebb2f87a993b129ad0abb5fa2
acdepak <acdepak@gmail.com>
2023-04-22 13:08:04 +0545

    fixed hover action and cheveron down icon's size

--- src/icons/CheveronDownIcon.tsx
+++ src/icons/CheveronDownIcon.tsx
@@ -1,4 +1,11 @@
-export const ChevronDownIcon =
-    <svg fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M6 9L12 15L18 9" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-    </svg>
+export const ChevronDownIcon = (
+  <svg fill="none" height={25} width={25} xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M6 9L12 15L18 9"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
