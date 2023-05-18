90d048b05afcd8740ed0d5e1498e9f52eba8ace8
acdepak <acdepak@gmail.com>
2023-05-18 22:59:52 +0545

    included onBlur to close the options

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -21,7 +21,7 @@ export default function Test({ locations, categories }) {
 
   const [showInputOptions, setshowInputOptions] = useState(false);
   const handleInputClick = () => {
-    setshowInputOptions(!showInputOptions);
+    setshowInputOptions(true);
   };
 
   const [click, setClick] = React.useState(false);
@@ -62,6 +62,7 @@ export default function Test({ locations, categories }) {
             onChange={onChange}
             placeholder="Search by category or location"
             onClick={handleInputClick}
+            onBlur={() => setshowInputOptions(false)}
           ></input>
         </div>
 
